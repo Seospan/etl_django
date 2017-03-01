d4316b6264dd8c36e14073a0e63daef34cee7560
Seospan <sophie.jobez@gmail.com>
2017-03-01 18:14:46 +0100

    Static urls

--- paramount_etl/paramount_etl/urls.py
+++ paramount_etl/paramount_etl/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
